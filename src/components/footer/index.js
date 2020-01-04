b56278b2f0ab9564a37f22bceb4397507a396913
Subham Sarkar <subhamsarkar2395.ss@gmail.com>
2020-01-04 13:43:08 +0530

    working on desktop design

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./index.scss";
+import FontAwesome from 'react-fontawesome'
+
 
 const Footer = () => {
   return (
     <footer className="footer">
-        <p>Footer Works !</p>
+       <div className="inner-footer">
+       		<p>© 2019 Dummy. All Rights Reserved.</p>
+       		<ul className="foot-social">
+       			<li><a href="#">
+       			<FontAwesome name="fab fa-facebook-square"/>
+       			</a></li>
+       			<li><a href="#">
+       			<FontAwesome name="fab fa-dribbble"/>
+
+       			</a></li>
+       			<li><a href="#">
+       			<FontAwesome name="fab fa-youtube"/>
+       			</a></li>
+       			<li><a href="#">
+       			<FontAwesome name="fab fa-twitter"/>
+       			</a></li>
+       		</ul>
+       </div>
     </footer>
   );
 };
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,13 @@ import "./index.scss";
 const Header = () => {
   return (
     <header className="header">
-        <p>Header Works !</p>
+        <div className="breadcums">
+        	<ul>
+        		<li><a href="#">Dashboard</a></li>
+        		<li><a href="#">Courses</a></li>
+        		<li className="current-menu-item"><a href="#">Browse Web Development</a></li>
+        	</ul>  
+        </div>
     </header>
   );
 };
