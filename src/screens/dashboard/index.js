a98b4874acce8c2dd85ae2ea30c17c40b741b226
kuldipkhamaru <kuldipkhamaru@gmail.com>
2020-01-04 17:27:58 +0530

    slider added

--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -6,101 +6,264 @@ import 'owl.carousel/dist/assets/owl.theme.default.css';
 import sliderImg from '../../assets/images/1.png';
 import FontAwesome from 'react-fontawesome'
 const Dashboard = () => {
-  return ( 
-    <div className = "container">
-    	<div className="progrss-course">
-    	 	<div className="inner-heading">
-    	 		<h3>Progress Courses</h3>
-    	 		<a href="#">View All</a>
-    	 	</div>	
-	 <OwlCarousel className="owl-theme" loop margin={30} nav dots={false} items={5}>
-	    <div class="item">
-	    	<div className="inner-items">
-	    		<figure>
-	    			<img src={sliderImg} alt="" />
-	    		</figure>
-	    		<div className="course-cont">
-	    			<h5>Learn The Complete Ruby on Rails Developer</h5>
-	    			<h6>3/20</h6>
-	    			<div class="course-progressbar">
-                        <div class="course-progressbar-filler" style={{width: "40%"}}></div>
-                    </div>
-	    		</div>
-	    	</div>
-	    </div>	
-	    <div class="item">
-	    	<div className="inner-items">
-	    		<figure>
-	    			<img src={sliderImg} alt="" />
-	    		</figure>
-	    		<div className="course-cont">
-	    			<h5>Learn The Complete Ruby on Rails Developer</h5>
-	    			<h6>3/20</h6>
-	    			<div class="course-progressbar">
-                        <div class="course-progressbar-filler" style={{width: "40%"}}></div>
-                    </div>
-	    		</div>
-	    	</div>
-	    </div>
-	    <div class="item">
-	    	<div className="inner-items">
-	    		<figure>
-	    			<img src={sliderImg} alt="" />
-	    		</figure>
-	    		<div className="course-cont">
-	    			<h5>Learn The Complete Ruby on Rails Developer</h5>
-	    			<h6>3/20</h6>
-	    			<div class="course-progressbar">
-                        <div class="course-progressbar-filler" style={{width: "40%"}}></div>
-                    </div>
-	    		</div>
-	    	</div>
-	    </div>
-	    <div class="item">
-	    	<div className="inner-items">
-	    		<figure>
-	    			<img src={sliderImg} alt="" />
-	    		</figure>
-	    		<div className="course-cont">
-	    			<h5>Learn The Complete Ruby on Rails Developer</h5>
-	    			<h6>3/20</h6>
-	    			<div class="course-progressbar">
-                        <div class="course-progressbar-filler" style={{width: "40%"}}></div>
-                    </div>
-	    		</div>
-	    	</div>
-	    </div>
-	    <div class="item">
-	    	<div className="inner-items">
-	    		<figure>
-	    			<img src={sliderImg} alt="" />
-	    		</figure>
-	    		<div className="course-cont">
-	    			<h5>Learn The Complete Ruby on Rails Developer</h5>
-	    			<h6>3/20</h6>
-	    			<div class="course-progressbar">
-                        <div class="course-progressbar-filler" style={{width: "40%"}}></div>
-                    </div>
-	    		</div>
-	    	</div>
-	    </div>
-	    <div class="item">
-	    	<div className="inner-items">
-	    		<figure>
-	    			<img src={sliderImg} alt="" />
-	    		</figure>
-	    		<div className="course-cont">
-	    			<h5>Learn The Complete Ruby on Rails Developer</h5>
-	    			<h6>3/20</h6>
-	    			<div class="course-progressbar">
-                        <div class="course-progressbar-filler" style={{width: "40%"}}></div>
-                    </div>
-	    		</div>
-	    	</div>
-	    </div>    
-	</OwlCarousel>
-		</div>
-     </div>
+  return (
+    <div className="container">
+      <div className="progrss-course">
+        <div className="inner-heading">
+          <h3>Progress Courses</h3>
+          <a href="#">View All</a>
+        </div>
+        <OwlCarousel className="owl-theme" loop margin={30} nav dots={false} items={5}>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </OwlCarousel>
+      </div>
+
+
+      <div className="progrss-course">
+        <div className="inner-heading">
+          <h3>Browse Web Development <span className="makeColor">Episodes</span></h3>
+          <a href="#">View All</a>
+        </div>
+        <OwlCarousel className="owl-theme" loop margin={30} nav dots={false} items={4}>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </OwlCarousel>
+      </div>
+
+
+      <div className="progrss-course">
+        <div className="inner-heading">
+          <h3>Browse Web Development <span className="makeColor">Courses</span></h3>
+          <a href="#">View All</a>
+        </div>
+        <OwlCarousel className="owl-theme" loop margin={30} nav dots={false} items={4}>
+
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div className="inner-items">
+              <figure>
+                <img src={sliderImg} alt="" />
+              </figure>
+              <div className="course-cont">
+                <h5>Learn The Complete Ruby on Rails Developer</h5>
+                <h6>3/20</h6>
+                <div class="course-progressbar">
+                  <div class="course-progressbar-filler" style={{ width: "40%" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </OwlCarousel>
+      </div>
+
+      
+    </div>
   );
 };
 
